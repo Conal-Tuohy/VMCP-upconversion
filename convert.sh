e271eb36b6e56b045d92b60e78b96a97e8225163
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2025-05-15 13:49:05 +1000

    update convert.sh

--- convert.sh
+++ convert.sh
@@ -5,15 +5,20 @@ XPROC="java -jar /usr/share/xmlcalabash-1.4.1-100/xmlcalabash-1.4.1-100.jar"
 echo `date`: Retrieving ODT files from GitHub...
 cd /home/RBGV_Admin/vmcp-odt/ && git pull
 
-echo `date`: Copying figure image files ...
-sudo mkdir -p /etc/xproc-z/vmcp/figure
-sudo cp "/usr/src/vmcp-word/images in letters/"*.jpg /etc/xproc-z/vmcp/figure/
+echo `date`: Removing  styles from  apparatus files
+pandoc -f odt -t markdown ./apparatus/editors-citations.odt | pandoc -f markdown -t odt -o ./letters/apparatus/editors-citations.odt
+pandoc -f odt -t markdown ./apparatus/honours-awards-memberships.odt | pandoc -f markdown -t odt -o ./letters/apparatus/honours-awards-memberships.odt
+pandoc -f odt -t markdown ./apparatus/muellers-publications.odt | pandoc -f markdown -t odt -o ./letters/apparatus/muellers-publications.odt
+
+echo `date`: Purging TEI
+rm -r /usr/src/xtf/data/tei/letters/
 
 echo `date`: Converting OpenDocument files to TEI format ...
 time $XPROC /usr/src/VMCP-upconversion/xproc/upconvert.xpl input-directory=/home/RBGV_Admin/vmcp-odt output-directory=/usr/src/xtf/data/tei
 
 echo `date`: Pushing changes in TEI to GitHub...
 cd /usr/src/xtf/data/tei
+mv letters/apparatus/* ./static/apparatus/
 git add .
 git commit -m "VMCP conversion pipeline run `date`"
 git push origin main
